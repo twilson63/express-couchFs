be4ea0dbde2ad6c87459214a357520b392da25da
Tom Wilson <tom@jackhq.com>
2018-08-01 09:09:01 -0400

    handled get inline and errors

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var express = require('express')
 var mime = require('mime')
 var multer = require('multer')
 var upload = multer({ dest: '/tmp' })
+const { path } = require('ramda')
 
 module.exports = function(config) {
   var app = express()
@@ -34,30 +35,26 @@ module.exports = function(config) {
     }
   }
 
-  // nano responds with entire request_defaults object. Let's prevent our
-  // credentials from making it all the way to the client
-  function sanitizeError(err) {
-    // console.log(err);
-    var redactKeys = ['auth', 'uri']
-    redactKeys.forEach(function(k) {
-      if (err.request[k]) {
-        err.request[k] = undefined
-      }
-      if (err.headers[k]) {
-        err.headers[k] = undefined
-      }
-    })
-
-    return err
-  }
-
   app.use(helmet())
 
-  app.get('/:name', function(req, res) {
+  app.get('/:name', async function(req, res) {
     var disposition = req.query.inline ? 'inline' : 'attachment'
+
     const db = getDb(req)
-    const s = db.attachment.getAsStream(req.params.name, 'file')
-    s.pipe(res)
+    const doc = await db
+      .get(req.params.name)
+      .catch(() => ({ name: 'file not found' }))
+    res.set(
+      'Content-Disposition',
+      disposition + '; filename="' + doc.name + '"'
+    )
+    if (path(['_attachments', 'file'], doc)) {
+      const s = db.attachment.getAsStream(req.params.name, 'file')
+      s.on('error', e => {})
+      s.pipe(res)
+    } else {
+      return res.status(404).send({ error: 'file not found' })
+    }
   })
 
   // handle file upload
--- test/get_test.js
+++ test/get_test.js
@@ -12,13 +12,58 @@ var response = {
 }
 
 nock('http://localhost:5984')
+  .get('/files/6f280dcbbc27bc80dabd4ea0e80027ff')
+  .reply(200, {
+    _id: '6f280dcbbc27bc80dabd4ea0e80027ff',
+    _rev: '1',
+    name: 'bar.jpg',
+    _attachments: {
+      file: {
+        'content-type': 'image/jpg'
+      }
+    }
+  })
+
+nock('http://localhost:5984')
   .get('/files/6f280dcbbc27bc80dabd4ea0e80027ff/file')
+  .reply(200, response)
+
+nock('http://localhost:5984')
+  .get('/files/6f280dcbbc27bc80dabd4ea0e80027ffa')
+  .reply(200, {
+    _id: '6f280dcbbc27bc80dabd4ea0e80027ffa',
+    _rev: '1',
+    name: 'foo.jpg',
+    _attachments: {
+      file: {
+        'content-type': 'image/jpg'
+      }
+    }
+  })
+
+nock('http://localhost:5984')
+  .get('/files/6f280dcbbc27bc80dabd4ea0e80027ffa/file')
   .reply(200, response)
 
 test('get image', t => {
   request(app)
     .get('/6f280dcbbc27bc80dabd4ea0e80027ff')
     .then(res => {
+      t.equals(
+        res.headers['content-disposition'],
+        'attachment; filename="bar.jpg"'
+      )
+      t.equals(res.statusCode, 200)
+      t.end()
+    })
+})
+
+test('get image inline', t => {
+  request(app)
+    .get('/6f280dcbbc27bc80dabd4ea0e80027ffa')
+    .query({ inline: true })
+    .then(res => {
+      t.equals(res.headers['content-disposition'], 'inline; filename="foo.jpg"')
       t.equals(res.statusCode, 200)
       t.end()
     })
